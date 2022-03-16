341c681b0bff571868c37ee5459d98f91446a2c4
Benoît Vandanjon <benoit@vandanjon.fr>
2022-03-16 14:58:30 +0100

    BAK save

---
+++ script.js
@@ -0,0 +1,12 @@
+/**
+ * redirect the 404.html to the index page after a 5sec delay
+ * @var string
+ */
+function redirection()
+{
+    setTimeout(function ()
+    {
+        window.location.href = "https://disrupt3d.github.io/error-404--projet-html-css--team-Alpha/"
+    },
+    5000);
+}
