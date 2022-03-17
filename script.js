c14190937163d2619c2cb2ed8330113cf79462e1
Benoît Vandanjon <benoit@vandanjon.fr>
2022-03-17 22:06:42 +0100

    base style with correct colors added to the main

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 /**
- * redirect the 404.html to the index page after a 10sec delay
+ * redirect the 404.html to the index page after a defined delay in seconds
  * @var string
  */
 function redirection()
 {
+    duration_in_seconds = 10;
+    time = duration_in_seconds * 1000 - 200;
+
     setTimeout(function ()
     {
         window.location.href = "https://disrupt3d.github.io/error-404--projet-html-css--team-Alpha/"
     },
-    10000);
+    time);
 }
